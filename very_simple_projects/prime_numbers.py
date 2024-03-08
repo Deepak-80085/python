dd8abd597086ab49c8e0373fc7222b0ed4f7d091
deepak <deepak.kumaran.10@gmail.com>
2024-03-08 10:59:22 +0530

    updated

--- very_simple_projects/prime_numbers.py
+++ very_simple_projects/prime_numbers.py
@@ -1,13 +1,13 @@
 
 def primenumbers(n):
-    for a in range(2, n//2 + 1):
+    for a in range(2, n//2):
         if n % a == 0:
             return False 
     return True 
 
 n1 = int(input("Enter n1: "))
 n2 = int(input("Enter n2: "))
-print(f"Prime numbers between {n1} and {n2} are: ", end="")
+print(f"Prime numbers between {n1} and {n2} are: ")
 for i in range(n1, n2 + 1):
     if primenumbers(i):
         print(f"| {i}",)
--- very_simple_projects/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-balance = y1 + y2 + y3 + y4 + y5
-print(f"The total amount of the products is {balance}")
